bb4439466ae31aa7a0d452fb3e88f5e6b1c85fa5
Paulo Antonio Mendes <thelukgaming333@gmail.com>
2025-01-09 06:34:57 -0300

    Commit antes de gravar refatoração dos repositórios

---
+++ lib/data/repositories/todos/todos_repository_dev.dart
@@ -0,0 +1,32 @@
+import 'package:mvvm/core/result/result.dart';
+import 'package:mvvm/data/repositories/todos/todos_repository.dart';
+import 'package:mvvm/domain/models/todo.dart';
+
+class TodosRepositoryDev implements TodosRepository {
+  final List<Todo> _todos = [];
+
+  @override
+  Future<Result<Todo>> add(String name) async {
+    final lastTodoIndex = _todos.length;
+
+    final Todo createdTodo = Todo(
+      id: lastTodoIndex + 1,
+      name: name,
+    );
+
+    return Result.ok(createdTodo);
+  }
+
+  @override
+  Future<Result<void>> delete(Todo todo) async {
+    if (_todos.contains(todo)) {
+      _todos.remove(todo);
+    }
+    return Result.ok(null);
+  }
+
+  @override
+  Future<Result<List<Todo>>> get() async {
+    return Result.ok(_todos);
+  }
+}
