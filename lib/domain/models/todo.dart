9de6b664fc719cb79efc590551a9b5bea66efe20
Paulo Antonio Mendes <thelukgaming333@gmail.com>
2025-01-18 07:45:14 -0300

    Primeiro método de services

--- lib/domain/models/todo.dart
+++ lib/domain/models/todo.dart
@@ -2,5 +2,12 @@ class Todo {
   final int id;
   final String name;
 
-  Todo({required this.id, required this.name});
+  Todo({
+    required this.id,
+    required this.name,
+  });
+
+  factory Todo.fromJson(Map<String, dynamic> json) {
+    return Todo(id: json["id"], name: json["name"]);
+  }
 }
