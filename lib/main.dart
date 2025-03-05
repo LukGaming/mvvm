509c2469bbccb3ddc435aca97aa42f1d8b98434a
Paulo Antonio Mendes <thelukgaming333@gmail.com>
2025-03-05 18:56:02 -0300

    Aula 01 de rotas e navegacao

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mvvm/data/repositories/todos/todos_repository_remote.dart';
-import 'package:mvvm/data/services/api/api_client.dart';
-import 'package:mvvm/ui/todo/viewmodels/todo_viewmodel.dart';
-import 'package:mvvm/ui/todo/widgets/todo_screen.dart';
+import 'package:mvvm/routing/router.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,21 +7,13 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         useMaterial3: false,
       ),
-      home: TodoScreen(
-        todoViewmodel: TodoViewmodel(
-          todosRepository: TodosRepositoryRemote(
-            apiClient: ApiClient(
-              host: "192.168.1.105",
-            ),
-          ),
-        ),
-      ),
+      routerConfig: routerConfig(),
     );
   }
 }
---
+++ lib/routing/router.dart
@@ -0,0 +1,28 @@
+import 'package:go_router/go_router.dart';
+import 'package:mvvm/data/repositories/todos/todos_repository_remote.dart';
+import 'package:mvvm/data/services/api/api_client.dart';
+import 'package:mvvm/routing/routes.dart';
+import 'package:mvvm/ui/todo/viewmodels/todo_viewmodel.dart';
+import 'package:mvvm/ui/todo/widgets/todo_screen.dart';
+
+GoRouter routerConfig() {
+  return GoRouter(
+    initialLocation: Routes.todos,
+    routes: [
+      GoRoute(
+        path: Routes.todos,
+        builder: (context, state) {
+          return TodoScreen(
+            todoViewmodel: TodoViewmodel(
+              todosRepository: TodosRepositoryRemote(
+                apiClient: ApiClient(
+                  host: "192.168.1.102",
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    ],
+  );
+}
