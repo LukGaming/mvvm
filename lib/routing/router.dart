006b5534ecda855a975001a060fff0091a456537
Paulo Antonio Mendes <thelukgaming333@gmail.com>
2025-03-30 14:25:31 -0300

    Aula de provider

--- lib/routing/router.dart
+++ lib/routing/router.dart
@@ -1,22 +1,12 @@
 import 'package:go_router/go_router.dart';
-import 'package:mvvm/data/repositories/todos/todos_repository_remote.dart';
-import 'package:mvvm/data/services/api/api_client.dart';
-import 'package:mvvm/domain/use_cases/todo_update_use_case.dart';
 import 'package:mvvm/routing/routes.dart';
 import 'package:mvvm/ui/todo/viewmodels/todo_viewmodel.dart';
 import 'package:mvvm/ui/todo/widgets/todo_screen.dart';
 import 'package:mvvm/ui/todo_details/viewmodels/todo_details_viewmodel.dart';
 import 'package:mvvm/ui/todo_details/widgets/todo_details_screen.dart';
+import 'package:provider/provider.dart';
 
 GoRouter routerConfig() {
-  final todosRepository = TodosRepositoryRemote(
-    apiClient: ApiClient(
-      host: "192.168.1.106",
-    ),
-  );
-
-  final todoUpdateCase = TodoUpdateUseCase(todosRepository: todosRepository);
-
   return GoRouter(
     initialLocation: Routes.todos,
     routes: [
@@ -25,8 +15,8 @@ GoRouter routerConfig() {
         builder: (context, state) {
           return TodoScreen(
             todoViewmodel: TodoViewmodel(
-              todosRepository: todosRepository,
-              todoUpdateUseCase: todoUpdateCase,
+              todosRepository: context.read(),
+              todoUpdateUseCase: context.read(),
             ),
           );
         },
@@ -37,8 +27,8 @@ GoRouter routerConfig() {
               final todoId = state.pathParameters["id"]!;
               final TodoDetailsViewModel todoDetailsViewModel =
                   TodoDetailsViewModel(
-                todosRepository: todosRepository,
-                todoUpdateUseCase: todoUpdateCase,
+                todosRepository: context.read(),
+                todoUpdateUseCase: context.read(),
               );
 
               todoDetailsViewModel.load.execute(todoId);
