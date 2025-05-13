b8ce5ce6e3daebfc22b130153d84fb2891527182
Paulo Antonio Mendes <thelukgaming333@gmail.com>
2025-05-13 06:24:07 -0300

    Aula de api_client 2

--- test/data/services/api_client_test.dart
+++ test/data/services/api_client_test.dart
@@ -4,15 +4,22 @@ import 'package:mvvm/data/services/api/models/todo/todo_api_model.dart';
 import 'package:mvvm/domain/models/todo.dart';
 import 'package:mvvm/utils/result/result.dart';
 
+import '../../mock/http_client_mock.dart';
+import '../../mock/todos.dart';
+
 void main() {
+  late MockHttpClient mockHttpClient;
   late ApiClient apiClient;
 
   setUp(() {
-    apiClient = ApiClient();
+    mockHttpClient = MockHttpClient();
+    apiClient = ApiClient(clientHttpFactory: () => mockHttpClient);
   });
 
   group("Should test [ApiClient]", () {
     test("Should return Result Ok when getTodos()", () async {
+      mockHttpClient.mockGet("/todos", mockGetTodos);
+
       final result = await apiClient.getTodos();
 
       expect(result.asOk.value, isA<List<Todo>>());
@@ -71,24 +78,56 @@ void main() {
       expect(result.asOk.value.done, true);
     });
 
-    test("Should getTodoById", () async {
-      CreateTodoApiModel todoToCreate = CreateTodoApiModel(
-        name: "Todo created on TEST ${DateTime.now()}",
+    test("Should delete a Todo when deleteTodo()", () async {
+      const CreateTodoApiModel todoToCreate = CreateTodoApiModel(
+        name: "Todo created on TEST",
         description: "Test description",
         done: false,
       );
 
       final createdTodoResult = await apiClient.postTodo(todoToCreate);
 
-      print("criando todo: ${createdTodoResult.asOk.value.toJson()}");
+      final result = await apiClient.deleteTodo(createdTodoResult.asOk.value);
 
-      final result =
-          await apiClient.getTodoById(createdTodoResult.asOk.value.id);
+      expect(result, isA<Result<void>>());
+    });
+
+    test("Should update a Todo when updateTodo()", () async {
+      const CreateTodoApiModel todoToCreate = CreateTodoApiModel(
+        name: "Todo created on TEST",
+        description: "Test description",
+        done: false,
+      );
+
+      final createdTodoResult = await apiClient.postTodo(todoToCreate);
+
+      final result = await apiClient.updateTodo(
+        UpdateTodoApiModel(
+          id: createdTodoResult.asOk.value.id!,
+          name:
+              "${createdTodoResult.asOk.value.name} updatedDate ${DateTime.now().toIso8601String()}",
+          description: "Test description",
+          done: true,
+        ),
+      );
+
+      expect(
+        result,
+        isA<Result<Todo>>(),
+      );
+
+      expect(result.asOk.value.done, true);
+    });
+
+    test("Should getTodoById", () async {
+      mockHttpClient.mockGet("/todos/1", mockGetById);
+
+      final result = await apiClient.getTodoById("1");
       expect(result, isA<Result<Todo>>());
 
-      expect(result.asOk.value.id, createdTodoResult.asOk.value.id);
+      final todo = result.asOk.value;
 
-      print(result.asOk.value.toJson());
+      expect(todo.id, "1");
     });
   });
 }
